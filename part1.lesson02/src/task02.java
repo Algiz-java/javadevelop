b77f47fac35af19ecc169a1310b7020227f51ce5
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-05 08:58:02 +1100

    task01,02,03 part01lesson02

---
+++ part1.lesson02/src/task02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class task02 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+            System.out.print("Введите вашу ЗП до вычета налога НДФЛ: ");
+            int num = in.nextInt();
+            float x;
+            x = (float) (num - (num * 0.13));
+            System.out.println("Ваша ЗП с вычетом НДФЛ 13% = " + x + " руб.");
+        }
+}
