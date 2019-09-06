bf37e607a1bf5586a1e7e036cbbdda351aeed07b
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-07 08:54:09 +1100

    Task#01 part1lesson3

--- part1/src/ru/akhmetshin/lesson03/GameHotCold.java
+++ part1/src/ru/akhmetshin/lesson03/GameHotCold.java
@@ -7,17 +7,18 @@ public class GameHotCold {
     public static void main(String[] args) {
         Random num = new Random();
         int x = num.nextInt(100);
-        //System.out.println(x);
         System.out.println("Угадай число от 1 до 100, у тебя есть 3 попытки");
         Scanner in = new Scanner(System.in);
         for (int i = 1; i < 4; i++){
-            System.out.print("Введите число, попытка № " + i + ":");
+            System.out.print("Введите число, попытка №" + i + ":");
             int y = in.nextInt();
             int diff = x - y;
-            if (Math.abs(diff) <= 5){
-                System.out.println("Горячо!");
+            if (Math.abs(diff) <= 10){
+                System.out.println("Горячо :)");
+                System.out.println("Загаданное число: " + x);
                 System.exit(0);
-            }else System.out.println("Холодно!");
+            }else System.out.println("Холодно :(");
         }
+        System.out.println("Загаданное число: " + x);
     }
 }
