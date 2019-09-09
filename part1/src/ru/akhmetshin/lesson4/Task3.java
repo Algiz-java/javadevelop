42b4713103c89ffd5a6bb9d1e20bc17fd40c3e99
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-10 10:51:53 +1100

    Task#1 - 4 part1lesson4

--- part1/src/ru/akhmetshin/lesson4/Task3.java
+++ part1/src/ru/akhmetshin/lesson4/Task3.java
@@ -1,14 +1,19 @@
 package ru.akhmetshin.lesson4;
 
 
-public class Task3 {
+import java.util.Formatter;
 
+public class Task3 {
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
-        System.out.println("Таблица умножения");
+        Formatter fmt = new Formatter();
+        fmt.format("%28s", "Таблица умножения");
+        System.out.println(ANSI_GREEN + fmt + ANSI_RESET);
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                    System.out.print(j * i + " " + "\t");
+                System.out.print(j * i + " " + "\t");
             }
             System.out.println();
         }
