b64144c1e743e2c55dc6de03668ff6aa1021eaca
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-09 22:07:08 +1100

    Task#01 part1lesson3

--- part1/src/ru/akhmetshin/lesson03/GameHotCold.java
+++ part1/src/ru/akhmetshin/lesson03/GameHotCold.java
@@ -7,18 +7,28 @@ public class GameHotCold {
     public static void main(String[] args) {
         Random num = new Random();
         int x = num.nextInt(100);
-        System.out.println("Угадай число от 1 до 100, у тебя есть 3 попытки");
+        System.out.println("Угадай загаданное число от 1 до 100!");
         Scanner in = new Scanner(System.in);
-        for (int i = 1; i < 4; i++){
-            System.out.print("Введите число, попытка №" + i + ":");
+        boolean i = true;
+        while (i){
+            System.out.print("Введите число:");
             int y = in.nextInt();
             int diff = x - y;
-            if (Math.abs(diff) <= 10){
-                System.out.println("Горячо :)");
-                System.out.println("Загаданное число: " + x);
+            if (x == y){
+                System.out.println("Конец игры, загаданное число: " + x);
                 System.exit(0);
-            }else System.out.println("Холодно :(");
+            }
+            if (Math.abs(diff) <= 10) {
+                System.out.print("Горячо :) " + "Хочешь продолжить игру, введи Y:");
+                String button = in.next();
+                i = button.equalsIgnoreCase("y");
+            }
+            if (Math.abs(diff) > 10) {
+                System.out.print("Холодно :( " + "Хочешь продолжить игру, введи Y:");
+                String button = in.next();
+                i = button.equalsIgnoreCase("y");
+            }
         }
-        System.out.println("Загаданное число: " + x);
+        System.out.println("Конец игры, загаданное число: " + x);
     }
 }
