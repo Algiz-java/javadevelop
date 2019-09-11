25547c4b7f26ba4852836ecbfa0658e35c125393
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-12 10:47:52 +1100

    Task#1 - 4 part1lesson4

--- part1/src/ru/akhmetshin/lesson04/Task2.java
+++ part1/src/ru/akhmetshin/lesson04/Task2.java
@@ -1,25 +1,29 @@
 package ru.akhmetshin.lesson04;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите любое число: ");
-        double x = in.nextDouble();
+        NumberFormat nf_in = NumberFormat.getNumberInstance(Locale.ENGLISH);
+        double z = nf_in.parse(in.next()).doubleValue();
         System.out.print("Это число: ");
-        if (x == 0){System.out.print("нулевое."); System.exit(0);}
+        if (z == 0){System.out.print("нулевое."); System.exit(0);}
 
-        if (x < 0){System.out.print("отрицательное, ");} else System.out.print("положительное, ");
+        if (z < 0){System.out.print("отрицательное, ");} else System.out.print("положительное, ");
 
-        if ((x - Math.floor(x)) == 0){
+        if ((z - Math.floor(z)) == 0){
             System.out.print("целое, ");
-            if ((x % 2) == 0){
+            if ((z % 2) == 0){
                 System.out.print("четное.");
             }else System.out.print("нечетное.");
         }
 
-        if ((x - Math.floor(x)) > 0){
+        if ((z - Math.floor(z)) > 0){
             System.out.print("дробное.");
         }
     }
