49bffc5e515aedbe85361e49f00ee2ab17fb0f3d
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-11 15:01:32 +1100

    Task#1 - 4 part1lesson4

--- part1/src/ru/akhmetshin/lesson04/Task3.java
+++ part1/src/ru/akhmetshin/lesson04/Task3.java
@@ -1,19 +1,21 @@
 package ru.akhmetshin.lesson04;
 
 
-import java.util.Formatter;
+import java.util.Scanner;
 
 public class Task3 {
     private static final String ANSI_GREEN = "\u001B[32m";
     private static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
-        Formatter fmt = new Formatter();
-        fmt.format("%28s", "Таблица умножения");
-        System.out.println(ANSI_GREEN + fmt + ANSI_RESET);
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.print(j * i + " " + "\t");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите ширину таблицы: ");
+        int x = in.nextInt(); //Есть ли функция считывания число после пробела, а не после клавиши Enter
+        System.out.printf(ANSI_GREEN + "%52s", "Таблица умножения" + ANSI_RESET + "\n"); /* Как здесь можно отцентровать
+        текст в зависимости от ширины таблицы умножения?*/
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= x; j++) {
+                System.out.printf("%5s", j * i + " " + "\t");
             }
             System.out.println();
         }
