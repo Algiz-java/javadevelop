8ae743fa51067e3a326d4d85952648244d680b43
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-14 17:21:21 +1100

    Part2 Lesson6

--- part2/src/lesson6/Main.java
+++ part2/src/lesson6/Main.java
@@ -7,21 +7,24 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        float itog = 0;
         String timeStamp = new SimpleDateFormat("dd/MM/yyyy").format(Calendar.getInstance().getTime());
         System.out.println("Отчет за "+ timeStamp + ":");
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) { //вывод 5 товаров с применением принципов ООП
             Product product = new Product();
-            Buy price = new Buy();
-            Tovar object = new Tovar();
+            Buy buy = new Buy();
+            Tovar tovar = new Tovar();
             Random random = new Random();
-            product.name = object.ObjectName;
+            product.name = tovar.ObjectName;
             product.val = random.nextInt(500);
             product.quant = random.nextInt(10);
-            price.z = product.quant;
-            price.check();
-            product.z = price.sums;
+            buy.z = product.quant;
+            buy.check();
+            product.z = buy.sums;
             product.PrintPrice();
             product.diff();
+            itog += buy.sums;
         }
+        System.out.printf("Общая прибыль: %s руб.%n", itog);
     }
 }
