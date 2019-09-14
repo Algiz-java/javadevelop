a6ee23d872834efb75841d00264e9df83e011987
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-14 18:00:04 +1100

    Part2 Lesson6

--- part2/src/lesson6/Product.java
+++ part2/src/lesson6/Product.java
@@ -9,12 +9,15 @@ class Product {
     int quant; //продано за день
 
     void PrintPrice() {
-        System.out.print(name + ": " + "\t" + "на складе " + val + "\t" + " продано " + quant + "\t"
-                + " прибыль за день " + z + " руб. " + "\t");
+        //System.out.format("%5s",name + ": " + "\t" + "на складе " + val + "\t" + " продано " + quant + "\t" + " прибыль за день " + z + " руб. " + "\t");
+        System.out.format("%-10s", name + ": "); //+ "\t" + "на складе " + val + "\t" + " продано " + quant + "\t" + " прибыль за день " + z + " руб. " + "\t");
+        System.out.format("%-10s", "на складе " + val);
+        System.out.format("%-10s", " продано " + quant );
+        System.out.format("%-10s", " прибыль за день " + z + " руб. ");
     }
 
     void diff() {
         int residue = val - quant; //сколько осталось в конце дня
-        System.out.println("остаток: " + residue + " шт.");
+        System.out.format("%-10s%n", "остаток: " + residue + " шт.");
     }
 }
