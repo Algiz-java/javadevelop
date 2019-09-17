9e6a1e16a5408d835c2e6468ee6d5c6c408f0144
Algiz Akhmetshin <algiz89@mail.ru>
2019-09-18 08:39:59 +1100

    Part2 Lesson7

--- part2/src/lesson7/Main.java
+++ part2/src/lesson7/Main.java
@@ -1,6 +1,8 @@
 package lesson7;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,11 +11,16 @@ public class Main {
         }
         int v = 500; //volume bottel for water in mL
         int cup = v / 100;//cups of coffee
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        String strDate = sdfDate.format(now);
         CalcEnum metodEnum = new CalcEnum();
         while (cup >= metodEnum.getCount()) {
             metodEnum.check();
         }
-        System.out.println("Итого: " + metodEnum.getSumma());
+        Date end = new Date();
+        String endDate = sdfDate.format(end);
+        System.out.println("Заработано с " + strDate + " по " + endDate + ": " + metodEnum.getSumma() + " руб.");
         System.out.println("Требуется обслуживание");
     }
 }
