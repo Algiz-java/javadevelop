bcd63a6c5ea289703dd06a6b8e36eab5c04ba925
Algiz Akhmetshin <algiz89@mail.ru>
2019-10-08 09:39:15 +0300

    part4 lesson15

--- part4/src/lesson15/FilesDirectory.java
+++ part4/src/lesson15/FilesDirectory.java
@@ -4,21 +4,41 @@ import java.io.File;
 
 public class FilesDirectory {
     public static void main(String[] args) {
-        showFiles(new File("E:\\Java8\\ru.akhmetshin\\part4\\"));
-    }
-    private static void showFiles(File f) {
-        File[] files = f.listFiles();
-        for(File p:files){
-            if (!p.isDirectory ()) {
-                System.out.println (p.getName ());
+        File file = new File("E:\\Java8\\ru.akhmetshin\\part4");
+        System.out.println(file.getAbsolutePath());
+        int dirrect = 0;
+        for (char c : file.getAbsolutePath().toCharArray()) {
+            if (c == '\\') {
+                dirrect++;
             }
-            if (p.isDirectory ()) {
+        }
+        showFiles(file, dirrect);
+    }
+
+    private static void showFiles(File f, int d) {
+        File[] files = f.listFiles();
+        int count = 0;
+        for (char c : f.getAbsolutePath().toCharArray()) {
+            if (c == '\\') {
+                count++;
+            }
+        }
+        int div = count - d;//разница в символе "\" между заданной дирректорией и анализируемой
+        assert files != null;
+        for (File p : files) {
+            if (p.isDirectory()) {
                 try {
-                    showFiles(p);
-                }
-                catch(Exception e){
+                        System.out.printf("%" + (div + 1) + "s", "\\");
+                        System.out.println(p.getName());
+
+                    showFiles(p, d);
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
+            }
+            if (p.isFile()) {
+                System.out.printf("%" + (div + 1) + "s", "");
+                System.out.println(p.getName() + " - file");
             }
         }
     }
