db750cbc98d45d05c114fad89c9578348bd428c2
Algiz Akhmetshin <algiz89@mail.ru>
2019-10-22 16:02:49 +1100

    part4 lesson20

---
+++ part4/src/lesson20/Json.java
@@ -0,0 +1,36 @@
+package lesson20;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Json {
+    private int userId;
+    private int id;
+    private String title;
+
+    int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+}
---
+++ part4/src/lesson20/ReadJsonURL.java
@@ -0,0 +1,45 @@
+package lesson20;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ReadJsonURL {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://jsonplaceholder.typicode.com/albums/20");
+            printConsole(url);
+            printClass(url);
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printConsole(URL url) {
+        try (InputStream is = url.openStream();
+             Reader reader = new InputStreamReader(is);
+             BufferedReader br = new BufferedReader(reader)){
+            String line;
+            while ((line = br.readLine())!=null){
+                System.out.print(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printClass(URL url) {
+        System.out.println("\n" + "Результат после Json.class:");
+        try (InputStream is = url.openStream();
+             Reader reader = new InputStreamReader(is)){
+            ObjectMapper mapper = new ObjectMapper();
+            Json json = mapper.readValue(reader, Json.class);
+            System.out.printf("ID: %d \nUserID: %d \nTitle: '%s'",json.getId(), json.getUserId(),json.getTitle());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
