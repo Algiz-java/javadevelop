05a04104dbdc4016f3433cf09104c75b3c788990
Algiz Akhmetshin <algiz89@mail.ru>
2019-10-24 13:03:22 +1100

    Part2 Lesson6

--- part2/src/lesson6/Stock.java
+++ part2/src/lesson6/Stock.java
@@ -6,12 +6,12 @@ class Stock {
     Stock() {
         products = new Product[5];
         TradePrice tradePrice = new TradePrice();
-        float percent = 1.2f;
-        products[0] = new Product("Apple", tradePrice.getApplePrice() * percent,4.5f);
-        products[1] = new Product("Orange", tradePrice.getOrangePrice() * percent,6.5f);
-        products[2] = new Product("Tomato",tradePrice.getTomatoPrice() * percent,10.4f);
-        products[3] = new Product("Beer",tradePrice.getBeerPrice() * percent,40f);
-        products[4] = new Product("Milk",tradePrice.getMilkPrice() * percent,20f);
+        final float PERCENT = 1.2f;
+        products[0] = new Product("Apple", tradePrice.getApplePrice() * PERCENT,4.5f);
+        products[1] = new Product("Orange", tradePrice.getOrangePrice() * PERCENT,6.5f);
+        products[2] = new Product("Tomato",tradePrice.getTomatoPrice() * PERCENT,10.4f);
+        products[3] = new Product("Beer",tradePrice.getBeerPrice() * PERCENT,40f);
+        products[4] = new Product("Milk",tradePrice.getMilkPrice() * PERCENT,20f);
     }
     float sumOfCost() {
         float sum = 0;
