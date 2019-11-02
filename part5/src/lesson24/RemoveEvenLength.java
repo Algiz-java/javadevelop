9eeda0494a40b6352f620be1366f4cf73a33bd35
Algiz Akhmetshin <algiz89@mail.ru>
2019-11-03 08:48:46 +1100

    part5 lesson24

--- part5/src/lesson24/RemoveEvenLength.java
+++ part5/src/lesson24/RemoveEvenLength.java
@@ -4,14 +4,7 @@ import java.util.Set;
 
 class RemoveEvenLength {
     Set<String> removeEvenLength(Set<String> set) {
-        String[] strings = set.toArray(new String[0]);
-        int size = set.size();
-        set.clear();
-        for (int i = 0; i < size; i++) {
-            if (strings[i].length() % 2 != 0) {
-                set.add(strings[i]);
-            }
-        }
+        set.removeIf(s -> s.length() % 2 == 0);
         return set;
     }
 }
