ea761d570aefd6d1f3a9c444a2c289f614a292c5
Algiz Akhmetshin <algiz89@mail.ru>
2019-11-15 18:01:02 +1100

    part5 lesson31

--- part5/src/lesson31/Palindrome.java
+++ part5/src/lesson31/Palindrome.java
@@ -8,27 +8,23 @@ public class Palindrome {
         System.out.print("Print your word for check: ");
         String str = in.nextLine();
         System.out.print("First way: ");
-        buildString(str);
+        System.out.println("It's palindrome? - " + buildString(str));
         System.out.print("Second way: ");
-        chrString(str);
+        System.out.print("It's palindrome? - " + chrString(str));
     }
 
-    private static void chrString(String str) {
+    private static boolean chrString(String str) {
         char[] chars = str.toCharArray();
         char[] reverseStr = new char[chars.length];
         for (int i = 0; i < chars.length; i++) {
             reverseStr[(chars.length - 1) - i] = chars[i];
         }
-        if (str.equalsIgnoreCase(String.valueOf(reverseStr))){
-             System.out.println("Palindrome - true, result: " + String.valueOf(reverseStr));
-        }else System.out.println("Palindrome - false, result: " + String.valueOf(reverseStr));
+        return str.equalsIgnoreCase(String.valueOf(reverseStr));
     }
 
-    private static void buildString(String str) {
+    private static boolean buildString(String str) {
         StringBuilder builder = new StringBuilder(str);
-        String reverseStr =builder.reverse().toString();
-        if (str.equalsIgnoreCase(reverseStr)) {
-            System.out.println("Palindrome - true, result: " + builder);
-        }else System.out.println("Palindrome - false, result: " + builder);
+        String reverseStr = builder.reverse().toString();
+        return str.equalsIgnoreCase(reverseStr);
     }
 }
