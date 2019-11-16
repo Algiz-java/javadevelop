cdd866c1b6dd5607a7e595e2ad8a9c81efdbcf1a
Algiz Akhmetshin <algiz89@mail.ru>
2019-11-16 18:35:04 +1100

    part5 lesson31

--- part5/src/lesson31/Palindrome.java
+++ part5/src/lesson31/Palindrome.java
@@ -8,22 +8,21 @@ public class Palindrome {
         System.out.print("Print your word for check: ");
         String str = in.nextLine();
         System.out.print("First way: ");
-        System.out.println("It's palindrome? - " + buildString(str));
+        System.out.println("It's palindrome? - " + isPalindrome2(str));
         System.out.print("Second way: ");
-        System.out.print("It's palindrome? - " + chrString(str));
+        System.out.print("It's palindrome? - " + isPalindrome1(str));
     }
 
-    //как корректно вывести результат этих методов и допустимо ли это вообще?
-    private static boolean chrString(String str) {
+    private static boolean isPalindrome1(String str) {
         char[] chars = str.toCharArray();
-        char[] reverseStr = new char[chars.length];
-        for (int i = 0; i < chars.length; i++) {
-            reverseStr[(chars.length - 1) - i] = chars[i];
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - i- 1])
+                return false;
         }
-        return str.equalsIgnoreCase(String.valueOf(reverseStr));
+        return true;
     }
 
-    private static boolean buildString(String str) {
+    private static boolean isPalindrome2(String str) {
         StringBuilder builder = new StringBuilder(str);
         String reverseStr = builder.reverse().toString();
         return str.equalsIgnoreCase(reverseStr);
